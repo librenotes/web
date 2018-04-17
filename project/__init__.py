53ad6c24524f4b1dee4fa627f3186d733c727321
Shubidubapp <shubidubapp@gmail.com>
2018-04-18 00:50:26 +0300

    login methods prepared

--- project/__init__.py
+++ project/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config as conf
 from flask.ext.cache import Cache
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(conf)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-
+login_manager = LoginManager()
+login_manager.init_app(app=app)
 db = SQLAlchemy(app)
 
 from project import controller
--- project/controller.py
+++ project/controller.py
@@ -1,13 +1,20 @@
-<<<<<<< HEAD
-from project import app, db
-from .models import User, Note, Category
-from flask import render_template
-=======
-from project import app, cache
-from flask import render_template, redirect, url_for, request
+from project import app, cache, login_manager
+from flask import render_template, redirect, url_for, request, session, flash
+from flask_login import login_user, current_user, login_required, logout_user
 from .forms import RegisterForm, LoginForm
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from Crypto.Cipher import AES
 
->>>>>>> 2599b24... created post methods for login and form
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), category='danger')
+
 
 @app.route("/")
 def index():
@@ -22,18 +29,28 @@ def register_get():
 @cache.cached()
 @app.route("/login", methods=["POST"])
 def login_get():
-    return render_template("login.html.j2")
+    form = LoginForm()
+    return render_template("login.html.j2", form=form)
 
 
 @app.route("/signin", methods=["POST"])
 def login_post():
     form = LoginForm(request.form)
     if form.validate():
-        
-    # TODO: check credentials
-    # TODO: decrypt note_encryption_key using password
-    # TODO: put decrypted note_encryption_key to session
-    return redirect(url_for('index'))
+        user_ = User.query.filter_by(username=form.username).first()
+        if user_ and check_password_hash(user_.password, form.password):
+            login_user(user_)
+            cipher = AES.new(form.password, AES.MODE_CBC)
+            rand_key = cipher.decrypt(user_.random_encrypted)
+            session['rand_key'] = rand_key
+            flash('Login Successful!', category='success')
+            return redirect(url_for(notes, user=form.username))
+        else:
+            flash('Password or Username does not match', category='danger')
+            return redirect(url_for(login_get))
+    else:
+        flash_errors(form)
+        return redirect(url_for(login_get))
 
 
 @app.route("/signup", methods=["POST"])
@@ -48,6 +65,7 @@ def register_post():
 @app.route("/notes/<user>")
 def notes(user):
     return render_template("notes.html.j2")
+
 
 @app.route("/dbc")
 def createdb():
