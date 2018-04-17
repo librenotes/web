26089163433cb7fd1fdf1324f0d5d8bf214a92d3
Ozan Onur Tek <ozanonurtek@gmail.com>
2018-04-18 00:54:15 +0300

    messageflash

--- project/controller.py
+++ project/controller.py
@@ -1,10 +1,11 @@
 from project import app, db
 from .models import User, Note, Category
-from flask import render_template
+from flask import render_template, flash
 
 @app.route("/")
 def index():
-    return render_template("index.html.j2")
+    flash("Message", "category")
+    return render_template("register.html.j2")
 
 @app.route("/login")
 def login():
