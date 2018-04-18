88b7979ff479977fde04696d0976ea40baf4a4ab
Ozan Onur Tek <ozanonurtek@gmail.com>
2018-04-18 19:53:46 +0300

    password-repassword validation

--- project/forms.py
+++ project/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, validators
 from wtforms.validators import InputRequired, Email
 from wtforms.fields.html5 import EmailField
 
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired('Please enter your username.')])
-    password = PasswordField("Password", validators=[InputRequired('Please enter your password.')])
+    password = PasswordField("Password", validators=[InputRequired('Please enter your password.'),  validators.EqualTo('re_password', message='Passwords must match')])
     re_password = PasswordField("Password Again", validators=[InputRequired('Please enter your password again.')])
     email = EmailField("Email", validators=[InputRequired('Please enter your email address.'),
                                             Email('This email address does not seem valid.')])
