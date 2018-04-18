944a44e45072cd9af655615cddca687581396a89
Ozan Onur Tek <ozanonurtek@gmail.com>
2018-04-18 03:51:38 +0300

    refactoring

--- project/controller.py
+++ project/controller.py
@@ -1,4 +1,4 @@
-from project import app, cache, db
+from project import app, cache, db, login_manager
 from flask import render_template, redirect, url_for, request, session, flash
 from flask_login import login_user, current_user, login_required, logout_user
 from .forms import RegisterForm, LoginForm
@@ -8,6 +8,11 @@ from Crypto.Cipher import AES
 from Crypto import Random
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
@@ -15,14 +20,6 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
             ), category='danger')
-
-
-def get_padded_passwords(password):
-    remainder = 16 - len(password) % 16
-    padded_plain = password + '0' * remainder
-    hashed_password = generate_password_hash(password)
-    padded_hashed = hashed_password[-16:]
-    return padded_plain, padded_hashed
 
 
 @app.route("/")
@@ -36,7 +33,6 @@ def register_get():
     return render_template("register.html.j2", form=form)
 
 
-@cache.cached()
 @app.route("/login", methods=["GET"])
 def login_get():
     form = LoginForm()
@@ -50,10 +46,7 @@ def login_post():
         user_ = User.query.filter_by(username=form.username.data).first()
         if user_ and check_password_hash(user_.password, form.password.data):
             login_user(user_)
-            padded_plain, padded_hashed = get_padded_passwords(form.password.data)
-            cipher = AES.new(padded_plain, AES.MODE_CBC, padded_hashed)
-            rand_key = cipher.decrypt(user_.random_encrypted)
-            session['rand_key'] = rand_key
+            session['rand_key'] = user_.get_random_key(form.password.data)
             flash('Login Successful!', category='success')
             return redirect(url_for('notes', user=form.username.data))
         else:
@@ -71,22 +64,16 @@ def register_post():
         user_ = User.query.filter_by(username=form.username.data).first()
         email_ = User.query.filter_by(email=form.email.data).first()
         if not (user_ or email_):   # check if username or email address already exists.
-            rand_key = Random.get_random_bytes(256)
-            padded_plain, padded_hashed = get_padded_passwords(form.password.data)
-            print(len(padded_hashed))
-            print(len(padded_plain))
-            cipher = AES.new(padded_plain, AES.MODE_CBC, padded_hashed)
-            random_encrypted = cipher.encrypt(rand_key)
             user = User()
+            user.create_rand_key(form.password.data)
             user.username = form.username.data
             user.password = generate_password_hash(form.password.data)
             user.email = form.email.data
-            user.random_hashed = generate_password_hash(rand_key)
-            user.random_encrypted = random_encrypted
             db.session.add(user)
             db.session.commit()
-            flash('Register Successful!', category='success')
-            return redirect(url_for('login_get'))
+            login_user(user)
+            flash('Register Successful, now you are logged in!', category='success')
+            return redirect(url_for('notes', user=form.username.data))
         else:
             flash('This username or email address is already in use', category='warning')
     else:
@@ -94,14 +81,14 @@ def register_post():
     return redirect(url_for('register_get'))
 
 
-@login_required
 @app.route("/notes/<user>")
+@login_required
 def notes(user):
     return render_template("notes.html.j2")
 
 
-@login_required
 @app.route("/logout")
+@login_required
 def logout():
     logout_user()
     return 'OK'
--- project/models.py
+++ project/models.py
@@ -1,4 +1,8 @@
 from project import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from Crypto.Cipher import AES
+from Crypto import Random
+
 
 categories_notes = db.Table(
     'categories_notes',
@@ -33,6 +37,26 @@ class User(db.Model):
     fk_notes = db.Column(db.Integer, db.ForeignKey(Note.id), nullable=True)
     notes = db.relationship(Note, backref='users')
 
+    def get_random_key(self, plain_pass):
+        padded_plain, padded_hashed = self.get_padded_passwords(plain_pass)
+        cipher = AES.new(padded_plain, AES.MODE_CBC, padded_hashed)
+        rand_key = cipher.decrypt(self.random_encrypted)
+        return rand_key
+
+    def get_padded_passwords(self, plain_pass):
+        remainder = 16 - len(plain_pass) % 16
+        padded_plain = plain_pass + '0' * remainder
+        hashed_password = generate_password_hash(plain_pass)
+        padded_hashed = hashed_password[-16:]
+        return padded_plain, padded_hashed
+
+    def create_rand_key(self, plain_pass):
+        rand_key = Random.get_random_bytes(256)
+        padded_plain, padded_hashed = self.get_padded_passwords(plain_pass)
+        cipher = AES.new(padded_plain, AES.MODE_CBC, padded_hashed)
+        random_encrypted = cipher.encrypt(rand_key)
+        self.random_encrypted = random_encrypted
+        self.random_hashed = generate_password_hash(rand_key)
 
     def is_authenticated(self):
         return True
