ab0e61a1bc4e2088f0633b47c8dee627a22d4973
Shubidubapp <shubidubapp@gmail.com>
2018-05-25 20:08:10 +0300

    import problem fixed

--- project/blueprints/login/forms.py
+++ project/blueprints/login/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired('Please enter your username.')])
     password = PasswordField("Password", validators=[InputRequired('Please enter your password.'),
-                                                     validators.Length(min=3, max=25, message="Your password length must be consist of at least 3, at most 25 characters.")])
+                                                     Length(min=3, max=25, message="Your password length must be consist of at least 3, at most 25 characters.")])
