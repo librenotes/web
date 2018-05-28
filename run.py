e361f03e088d4490dd3ba0037acb0e26605e98af
ozanonurtek <ozanonurtek@gmail.com>
2018-05-28 21:42:57 +0300

    minor fixes on wsgi, cache time increased to 30 mins

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from project import create_app
 from project.config import Config as config
 
+app = create_app(config)
+
 if __name__ == "__main__":
-    app = create_app(config)
     app.run(host="0.0.0.0")
